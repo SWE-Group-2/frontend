dfb30a77e10773af7e1816c5550cd5f8792e8b27
mythicalbadger <jussy12.work@gmail.com>
2024-03-20 17:59:11 +0700

    feat: redirect to login if not authenticated

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,45 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {createRouter, createWebHistory} from "vue-router";
+import {isLoggedIn} from "@/services/AuthService.spec.js";
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    {
-      path: "/",
-      component: () => import("../views/HomeView.vue"),
-    },
-    {
-      path: "/login",
-      component: () => import("../views/LoginView.vue"),
-    },
-    {
-      path: "/register",
-      component: () => import("../views/RegisterView.vue"),
-    },
-    {
-      path: "/internships",
-      component: () => import("../views/InternshipListView.vue"),
-    },
-    {
-      path: "/students",
-      component: () => import("../views/StudentsListView.vue"),
-    },
-    {
-      path: "/edit-internship",
-      component: () => import("../views/EditPostView.vue"),
-    },
-  ],
+    history: createWebHistory(),
+    routes: [
+        {
+            path: "/",
+            component: () => import("../views/HomeView.vue"),
+        },
+        {
+            path: "/login",
+            component: () => import("../views/LoginView.vue"),
+        },
+        {
+            path: "/register",
+            component: () => import("../views/RegisterView.vue"),
+        },
+        {
+            path: "/internships",
+            component: () => import("../views/InternshipListView.vue"),
+        },
+        {
+            path: "/students",
+            component: () => import("../views/StudentsListView.vue"),
+        },
+        {
+            path: "/edit-internship",
+            component: () => import("../views/EditPostView.vue"),
+        },
+    ],
 });
 
+router.beforeEach((to, from, next) => {
+    const publicPages = ["/", "/internships", "/login", "/register", "/students"];
+    const authRequired = !publicPages.includes(to.path);
+    const loggedIn = isLoggedIn();
+
+    if (authRequired && !loggedIn) {
+        next('/login');
+    } else {
+        next();
+    }
+});
 export default router;
