6f12f934a3e18b73c1a5154e00e624ca6d802f4e
mythicalbadger <jussy12.work@gmail.com>
2024-03-22 11:16:37 +0700

    feat: endpoints enum

---
+++ src/constants/endpoints.ts
@@ -0,0 +1,7 @@
+export enum Endpoints {
+  USERS = "/users",
+  REGISTER = "/users/register",
+  LOGIN = "/users/login",
+  GET_CURRENT_USER = "/users/get_current_user",
+  GET_USER_BY_ID = "/users/{user_id}",
+}
