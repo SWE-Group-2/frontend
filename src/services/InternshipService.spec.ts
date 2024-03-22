d0ac89bda59685bc52a223206c98a80fee90d71e
mythicalbadger <jussy12.work@gmail.com>
2024-03-22 18:39:49 +0700

    feat: internship create type

---
+++ src/services/InternshipService.spec.ts
@@ -0,0 +1,26 @@
+import {Endpoints} from "@/constants/endpoints";
+import HttpClient from "@/services/HttpClient";
+import {InternshipCreate} from "@/types/InternshipCreate";
+
+/**
+ * Creates a new internship.
+ * @param internship the information of the internship
+ * @returns success message
+ * @throws error if the request body is invalid, the user is unauthorized, or the creation fails
+ */
+export async function createInternship(internship: InternshipCreate) {
+    const response = await HttpClient.post(Endpoints.CREATE_INTERNSHIP, internship, true);
+
+    if (response.ok) {
+        return response.json();
+    }
+
+    switch (response.status) {
+        case 400:
+            throw new Error("Invalid request body");
+        case 401:
+            throw new Error("Unauthorized");
+        default:
+            throw new Error("Failed to create internship");
+    }
+}
