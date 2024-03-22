d97e0dbef1bee9fedfbb581375aae3d357680410
mythicalbadger <jussy12.work@gmail.com>
2024-03-22 23:25:40 +0700

    test: shitty create internship tests

---
+++ src/components/createInternship/CreateInternship.spec.ts
@@ -0,0 +1,21 @@
+import { mount } from '@vue/test-utils'
+import { describe, test, expect } from "vitest";
+import { useCreateInternship } from "@/components/createInternship/CreateInternship.vue";
+
+
+describe('CreateInternship', () => {
+    test('Can mount', () => {
+        const wrapper = mount(useCreateInternship);
+        expect(wrapper.exists()).toBe(true);
+    });
+
+    test("Internal state test", () => {
+        const createInternship = useCreateInternship();
+        expect(createInternship.company.value).toBe("");
+        expect(createInternship.position.value).toBe("");
+        expect(createInternship.website.value).toBe("");
+        expect(createInternship.deadline.value).toBe("2024-01-01");
+        expect(createInternship.timePeriodId.value).toBe(1);
+        expect(createInternship.companyPhotoLink.value).toBe(null);
+    });
+});
