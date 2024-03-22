163e606f6deb8aacfc129b607e831f97982987dd
mythicalbadger <jussy12.work@gmail.com>
2024-03-22 18:37:32 +0700

    feat: create internship endpoint

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -4,4 +4,5 @@ export enum Endpoints {
   LOGIN = "/users/login",
   GET_CURRENT_USER = "/users/get_current_user",
   GET_USER_BY_ID = "/users/{user_id}",
+  CREATE_INTERNSHIP = "/internships/add_internship",
 }
