b0be29db48d26ff7e0a10de58eec7bf3cb1ccd8b
mythicalbadger <jussy12.work@gmail.com>
2024-03-24 01:16:11 +0700

    feat: edit internship method

--- src/services/Internship.service.ts
+++ src/services/Internship.service.ts
@@ -1,6 +1,7 @@
 import { Endpoints } from "@/constants/endpoints";
 import HttpClient from "@/services/HttpClient";
 import { InternshipCreate } from "@/types/InternshipCreate";
+import formatEndpoint from "@/utils/formatEndpoint";
 
 /**
  * Creates a new internship.
@@ -35,6 +36,43 @@ export async function createInternship(internship: InternshipCreate) {
       throw new Error("Unauthorized");
     default:
       throw new Error("Failed to create internship");
+  }
+}
+
+export async function editInternship(
+  internshipId: number,
+  internship: InternshipCreate,
+) {
+  const internshipJson = {
+    company: internship.company,
+    position: internship.position,
+    website: internship.website,
+    deadline: internship.deadline,
+    time_period_id: internship.timePeriodId,
+    company_photo_link: internship.companyPhotoLink,
+  };
+
+  const response = await HttpClient.put(
+    formatEndpoint(Endpoints.INTERNSHIP_BY_ID, {
+      internship_id: internshipId.toString(),
+    }),
+    internshipJson,
+    true,
+  );
+
+  if (response.ok) {
+    return response.json();
+  }
+
+  switch (response.status) {
+    case 400:
+      throw new Error("Invalid request body");
+    case 401:
+      throw new Error("Unauthorized");
+    case 404:
+      throw new Error("Internship not found");
+    default:
+      throw new Error("Failed to edit internship");
   }
 }
 
