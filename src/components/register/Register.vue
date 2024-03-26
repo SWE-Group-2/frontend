b507e0708749337c3665202bf9cba06bb038c938
Aethyar <aethyar@gmail.com>
2024-03-26 12:26:13 +0700

    Refactor: Register view now uses stateless components

---
+++ src/components/register/Register.vue
@@ -0,0 +1,66 @@
+<script lang="ts">
+import { Ref, ref } from "vue";
+import { registerUser } from "@/services/User.service";
+import router from "@/router";
+import { setAuthToken } from "@/services/Auth.service";
+
+// The state of the register page
+export interface RegisterPageState {
+  first_name: Ref<string>;
+  last_name: Ref<string>;
+  username: Ref<string>;
+  password: Ref<string>;
+  confirm_password: Ref<string>;
+  submit: () => Promise<void>;
+}
+
+export function useRegisterPage(): RegisterPageState {
+  const first_name = ref<string>("");
+  const last_name = ref<string>("");
+  const username = ref<string>("");
+  const password = ref<string>("");
+  const confirm_password = ref<string>("");
+
+  // Parent component will handle the submission of the form
+  async function submit() {
+    if (password.value !== confirm_password.value) {
+      console.error("Passwords do not match");
+      return;
+    }
+
+    const UserRegistrationInfo = {
+      first_name: first_name.value,
+      last_name: last_name.value,
+      username: username.value,
+      password: password.value,
+    };
+    const response = await registerUser(UserRegistrationInfo);
+    setAuthToken(response["access_token"]);
+    await router.push("/login");
+  }
+
+  return {
+    first_name,
+    last_name,
+    username,
+    password,
+    confirm_password,
+    submit,
+  };
+}
+</script>
+<script setup lang="ts">
+import RegisterView from "@/components/register/RegisterView.vue";
+
+const registerPage = useRegisterPage();
+</script>
+<template>
+  <RegisterView
+    v-model:first_name="registerPage.first_name.value"
+    v-model:last_name="registerPage.last_name.value"
+    v-model:username="registerPage.username.value"
+    v-model:password="registerPage.password.value"
+    v-model:confirm_password="registerPage.confirm_password.value"
+    @submit="registerPage.submit"
+  ></RegisterView>
+</template>
---
+++ src/components/register/RegisterView.vue
@@ -0,0 +1,89 @@
+<script setup lang="ts">
+// The state of the register page
+defineProps<{
+  first_name: string;
+  last_name: string;
+  username: string;
+  password: string;
+  confirm_password: string;
+}>();
+
+// Events that can occur on register, i.e., submission of the form
+// No methods here but rather the view informs parent component of an event occurring
+defineEmits<{
+  (e: "submit"): void;
+  (e: "update:first_name", value: string): void;
+  (e: "update:last_name", value: string): void;
+  (e: "update:username", value: string): void;
+  (e: "update:password", value: string): void;
+  (e: "update:confirm_password", value: string): void;
+}>();
+</script>
+<template>
+  <div class="item-center">
+    <div class="register-box align-center">
+      <h1 class="h1-text">REGISTER</h1>
+      <form @submit.prevent="$emit('submit')">
+        <div class="input-group">
+          <input
+            id="first_name"
+            :value="first_name"
+            @input="$emit('update:first_name', $event.target.value)"
+            class="input-text"
+            type="text"
+            placeholder="first name"
+            required
+          />
+        </div>
+        <div class="input-group">
+          <input
+            id="last_nname"
+            :value="last_name"
+            @input="$emit('update:last_name', $event.target.value)"
+            class="input-text"
+            type="text"
+            placeholder="last name"
+            required
+          />
+        </div>
+        <div class="input-group">
+          <input
+            id="username"
+            :value="username"
+            @input="$emit('update:username', $event.target.value)"
+            class="input-text"
+            type="text"
+            placeholder="username"
+            required
+          />
+        </div>
+        <div class="input-group">
+          <input
+            id="password"
+            :value="password"
+            @input="$emit('update:password', $event.target.value)"
+            class="input-text"
+            type="password"
+            placeholder="password"
+            required
+          />
+        </div>
+        <div class="input-group">
+          <input
+            id="confirmpassword"
+            :value="confirm_password"
+            @input="$emit('update:confirm_password', $event.target.value)"
+            class="input-text"
+            type="password"
+            placeholder="confirm password"
+            required
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  </div>
+</template>
+<style scoped>
+@import "@/components/register/Register.css";
+</style>
