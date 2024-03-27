1cc10bfcdfff0521122365960d17b48a17379979
Aethyar <aethyar@gmail.com>
2024-03-27 17:05:32 +0700

    Style: More information shown on Internship view
    - currently uses placeholders for author information

--- src/components/viewInternship/ViewInternshipView.vue
+++ src/components/viewInternship/ViewInternshipView.vue
@@ -71,23 +71,50 @@ function getTimePeriodName(id: number): string {
       </div>
       <div class="line"></div>
       <div class="body">
-        <div class="row">
-          <span class="title">Time Period:</span>
-          <span>
-            {{ getTimePeriodName(internship.time_period_id) }}
-          </span>
+        <div class="body-1">
+          <div class="row">
+            <span class="title">Time Period:</span>
+            <span>
+              {{ getTimePeriodName(internship.time_period_id) }}
+            </span>
+          </div>
+          <div class="row">
+            <span class="title">Deadline:</span>
+            <span>
+              {{ internship.deadline }}
+            </span>
+          </div>
+          <div class="row">
+            <span class="title">Apply on:</span>
+            <a
+              class="link"
+              :href="internship.website"
+              style="color: inherit; text-decoration: none"
+              :title="internship.website"
+            >
+              {{ internship.website }}
+            </a>
+          </div>
         </div>
-        <div class="row">
-          <span class="title">Deadline:</span>
-          <span>
-            {{ internship.deadline }}
-          </span>
-        </div>
-        <div class="row">
-          <span class="title">Apply on:</span>
-          <span class="link">
-            {{ internship.website }}
-          </span>
+        <div class="body-2">
+          <div class="row">
+            <span class="title">Author:</span>
+            <span> {{ internship.author_id }} </span>
+            <!-- placeholder -->
+          </div>
+          <div class="row">
+            <span class="title">Author role:</span>
+            <span>
+              Student
+              <!-- placeholder -->
+            </span>
+          </div>
+          <div class="row">
+            <span class="title">Posted on:</span>
+            <span>
+              {{ internship.created_at }}
+            </span>
+          </div>
         </div>
       </div>
     </div>
