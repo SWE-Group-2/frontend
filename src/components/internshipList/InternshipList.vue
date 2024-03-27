16cc06e1b4b84d1f582f1a0277edff8da498f48b
mythicalbadger <jussy12.work@gmail.com>
2024-03-27 16:00:09 +0700

    feat: shitty filter

--- src/components/internshipList/InternshipList.vue
+++ src/components/internshipList/InternshipList.vue
@@ -4,15 +4,25 @@ import { ref, Ref } from "vue";
 import { getAllInternships } from "@/services/Internship.service";
 
 type InternshipSortKey = "deadline" | "created_at";
+type InternshipFilterKey = "position" | "company" | "time_period" | "flagged";
 
 export interface InternshipListState {
   internships: Ref<Internship[]>;
+  internshipFilterKey: Ref<string>;
+  internshipFilterSearchTerm: Ref<string>;
   loadInternships: () => Promise<void>;
   sortInternships: (sortKey: InternshipSortKey) => void;
+  filterInternships: (
+    filterKey: InternshipFilterKey,
+    searchTerm: string,
+  ) => void;
+  resetInternships: () => void;
 }
 
 export function useInternshipList(): InternshipListState {
   const internships = ref<Internship[]>([]);
+  const internshipFilterKey = ref<string>("");
+  const internshipFilterSearchTerm = ref<string>("");
 
   async function loadInternships() {
     await sortInternships("created_at");
@@ -27,10 +37,28 @@ export function useInternshipList(): InternshipListState {
     });
   }
 
+  async function filterInternships() {
+    const fetchedInternships: Internship[] = await getAllInternships();
+    internships.value = fetchedInternships.filter((internship) => {
+      return internship[internshipFilterKey.value]
+        .toString()
+        .toLowerCase()
+        .includes(internshipFilterSearchTerm.value.toLowerCase());
+    });
+  }
+
+  async function resetInternships() {
+    await loadInternships();
+  }
+
   return {
     internships,
+    internshipFilterKey,
+    internshipFilterSearchTerm,
     loadInternships,
     sortInternships,
+    filterInternships,
+    resetInternships,
   };
 }
 </script>
@@ -43,8 +71,16 @@ const internshipListPage = useInternshipList();
   <Suspense>
     <InternshipListView
       v-model:internships="internshipListPage.internships.value"
+      v-model:internship-filter-key="
+        internshipListPage.internshipFilterKey.value
+      "
+      v-model:internship-filter-search-term="
+        internshipListPage.internshipFilterSearchTerm.value
+      "
       @loadInternships="internshipListPage.loadInternships"
       @sort-internships="internshipListPage.sortInternships"
+      @filter-internships="internshipListPage.filterInternships"
+      @reset-internships="internshipListPage.resetInternships"
     ></InternshipListView>
   </Suspense>
 </template>
--- src/components/internshipList/InternshipListView.vue
+++ src/components/internshipList/InternshipListView.vue
@@ -6,12 +6,18 @@ import { TimePeriod } from "@/types/TimePeriod";
 // State of the view -- the list of internships
 defineProps<{
   internships: Internship[];
+  internshipFilterKey: string;
+  internshipFilterSearchTerm: string;
 }>();
 
 // Events of the view -- the loading of internships
 const emit = defineEmits<{
   (e: "loadInternships"): void;
   (e: "sortInternships", sortKey: string): void;
+  (e: "filterInternships"): void;
+  (e: "resetInternships"): void;
+  (e: "update:internshipFilterKey", value: string): void;
+  (e: "update:internshipFilterSearchTerm", value: string): void;
 }>();
 
 // Call the loadInternships event to cause parent component to load internships
@@ -37,11 +43,55 @@ function getTimePeriodName(id: number): string {
       </RouterLink>
     </div>
     <div class="sort-button-container">
-      <select @change="emit('sortInternships', $event.target.value)">
+      <select @change="$emit('sortInternships', $event.target.value)">
         <option value="created_at">Sort by Created At</option>
         <option value="deadline">Sort by Deadline</option>
       </select>
     </div>
+    <div class="filter-internship-container">
+      <select
+        @change="$emit('update:internshipFilterKey', $event.target.value)"
+        :value="internshipFilterKey"
+      >
+        <option value="position">Position</option>
+        <option value="company">Company</option>
+        <option value="time_period">Time Period</option>
+        <option value="flagged">Flagged</option>
+      </select>
+      <input
+        v-if="
+          internshipFilterKey === 'position' ||
+          internshipFilterKey === 'company'
+        "
+        type="text"
+        :value="internshipFilterSearchTerm"
+        @input="$emit('update:internshipFilterSearchTerm', $event.target.value)"
+      />
+      <select
+        v-if="internshipFilterKey === 'time_period'"
+        :value="internshipFilterSearchTerm"
+        @change="
+          $emit('update:internshipFilterSearchTerm', $event.target.value)
+        "
+      >
+        <option v-for="timePeriod in timePeriods" :value="timePeriod.name">
+          {{ timePeriod.name }}
+        </option>
+      </select>
+      <select
+        v-if="internshipFilterKey === 'flagged'"
+        :value="internshipFilterSearchTerm"
+        @change="
+          $emit('update:internshipFilterSearchTerm', $event.target.value)
+        "
+      >
+        <option value="true">Flagged</option>
+        <option value="false">Not Flagged</option>
+      </select>
+      <button @click="$emit('filterInternships')">Filter</button>
+      <button @click="$emit('resetInternships')">Reset</button>
+    </div>
+
     <div id="internships">
       <div
         class="student-container"
