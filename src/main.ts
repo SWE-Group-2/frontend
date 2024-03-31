fdb2673f58fe76cea5b988812210f467ab51acd4
mythicalbadger <jussy12.work@gmail.com>
2024-03-31 11:03:49 +0700

    refactor: setup for google login

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,13 @@ import "./style.css";
 
 import { createApp } from "vue";
 import App from "./App.vue";
+import vue3GoogleLogin from "vue3-google-login";
 
 import router from "./router";
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+app.use(vue3GoogleLogin, {
+    clientId: import.meta.env.VITE_GOOGLE_CLIENT_ID
+})
+app.use(router);
+app.mount("#app");
