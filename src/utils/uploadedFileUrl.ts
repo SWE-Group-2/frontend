fdc96a5d826b0788839185ccd0322759060d5fe0
mythicalbadger <jussy12.work@gmail.com>
2024-04-03 13:02:05 +0700

    feat: util for profile picture url

---
+++ src/utils/uploadedFileUrl.ts
@@ -0,0 +1,5 @@
+export function getProfilePictureUrl(userId: number): string {
+    const bucketName = import.meta.env.VITE_AWS_BUCKET_NAME_PROFILEPICS;
+    const bucketRegion = import.meta.env.VITE_AWS_DEFAULT_REGION;
+    return `https://${bucketName}.s3.${bucketRegion}.amazonaws.com/profilepic_${userId}.png`;
+}
