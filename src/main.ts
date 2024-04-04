7608b338ff8ed55f16d3ddcd9e8d6b53400218cf
mythicalbadger <jussy12.work@gmail.com>
2024-04-04 18:09:54 +0700

    more logging

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ import router from "./router";
 
 const app = createApp(App);
 console.log("UNGABUGNA");
+console.log(process.env);
+console.log(import.meta.env);
 console.log(import.meta.env.VITE_GOOGLE_CLIENT_ID);
 console.log(process.env.VUE_APP_ENV_VARIABLE);
 console.log(process.env.VITE_GOOGLE_CLIENT_ID);
