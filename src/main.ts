a74bfa86d0492921b5542a9f87790f0540cbf777
mythicalbadger <jussy12.work@gmail.com>
2024-04-04 18:24:04 +0700

    more logging

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ console.log(import.meta.env);
 console.log(import.meta.env.VITE_GOOGLE_CLIENT_ID);
 console.log(process.env.VUE_APP_ENV_VARIABLE);
 console.log(process.env.VITE_GOOGLE_CLIENT_ID);
+console.log("OOOGABOOGA");
 const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID ?? process.env.VUE_APP_ENV_VARIABLE;
 app.use(vue3GoogleLogin, {
     clientId: clientId
