4050ca62cb7277d7a73464b08d1755fe1d06f381
mythicalbadger <jussy12.work@gmail.com>
2024-04-04 19:53:10 +0700

    NOW YOU BETTER WORK YOU FUCKER

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,6 @@ import vue3GoogleLogin from "vue3-google-login";
 import router from "./router";
 
 const app = createApp(App);
-console.log(import.meta.env);
-console.log(import.meta.env.VITE_GOOGLE_CLIENT_ID);
 const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 app.use(vue3GoogleLogin, {
     clientId: clientId
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
+import {defineConfig, loadEnv} from "vite";
 import vue from "@vitejs/plugin-vue";
 import * as path from "path";
 
-// https://vitejs.dev/config/
+const env = loadEnv("", process.cwd());
 export default defineConfig({
   plugins: [vue()],
   resolve: {
@@ -17,6 +17,6 @@ export default defineConfig({
     },
   },
   define: {
-    'import.meta.env.VITE_GOOGLE_CLIENT_ID': 'process.env.VITE_GOOGLE_CLIENT_ID'
+    'import.meta.env.VITE_GOOGLE_CLIENT_ID': JSON.stringify(env.VITE_GOOGLE_CLIENT_ID),
   }
 });
